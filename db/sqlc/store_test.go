c80738f3fe15d76c0def6aa448552dbb91a347c7
hayat711 <71981169+hayat711@users.noreply.github.com>
2023-11-25 16:24:55 +0900

    modified .gitignore

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -25,8 +25,8 @@ func TestTransferTx(t *testing.T) {
 		go func() {
 
 			result, err := store.TransferTx(context.Background(), TransferTxParams{
-				FromAccountId: account1.ID,
-				ToAccountId:   account2.ID,
+				FromAccountID: account1.ID,
+				ToAccountID:   account2.ID,
 				Amount:        amount,
 			})
 			errs <- err
@@ -136,8 +136,8 @@ func TestTransferTxDeadLock(t *testing.T) {
 
 		go func() {
 			_, err := store.TransferTx(context.Background(), TransferTxParams{
-				FromAccountId: fromAccountID,
-				ToAccountId:   toAccountID,
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
 				Amount:        amount,
 			})
 			errs <- err
